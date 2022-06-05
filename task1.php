395f7284dcd893cfdea5d5f01deb1c34ebbc9c05
Mariham Fouad Halim <103039445+MarihamF@users.noreply.github.com>
2022-06-06 01:46:33 +0200

    Add files via upload

---
+++ task1.php
@@ -0,0 +1,10 @@
+<?php
+
+$num1 = 10; $num2 = 20;
+ 
+$num2 = $num2 - $num1;
+$num1 = $num1 + $num2; 
+ 
+ 
+echo "num1 = ",$num1, ", ", "num2 = ", $num2;
+?>
---
+++ task2.php
@@ -0,0 +1,5 @@
+<?php
+$message_1 = "Hello";
+$message_2 = "This is the first task";
+echo $message_1." ". $message_2;
+?>
