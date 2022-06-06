752ad4d3dd7f69367a6e009fb0dc8f6b5838b308
Mariham Fouad Halim <103039445+MarihamF@users.noreply.github.com>
2022-06-07 00:40:31 +0200

    Add files via upload

---
+++ task3.php
@@ -0,0 +1,22 @@
+<?php
+
+ $letter = 'a';
+
+ $next_letter = ++$letter; 
+
+ if (strlen($next_letter) > 1) 
+
+ {
+  $next_letter = $next_letter[0];
+  }
+
+ echo $next_letter."\n";
+?>
+
+
+
+
+
+
+
+
